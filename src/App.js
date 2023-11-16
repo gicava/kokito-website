f707883944d23161b03586425bcd9babb4baef9b
gicava <gerardo.cavazos01@utrgv.edu>
2023-11-15 18:09:07 -0600

    reworked featured rolls

--- src/App.js
+++ src/App.js
@@ -5,24 +5,50 @@ import EntreeAndSidesSection from './components/EntreeAndSidesSection';
 import HomePage from './components/HomePage';
 import Logo from './assets/KokitoTitle.png'
 import SectionBtn from './components/SectionBtn';
+import SushiRollsSection from './components/SushiRollsSection';
+
+import KokitoRoll from './assets/Kokito/sushi/kokito roll.jpg'
+import RubiRoll from './assets/Kokito/sushi/rubi roll.jpg'
+import MaresRoll from './assets/Kokito/sushi/mares roll.JPEG'
+import CaterpillarRoll from './assets/Kokito/sushi/caterpillar roll.JPG'
+import FijiRoll from './assets/Kokito/sushi/fiji roll.jpg'
+
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home');  // Set the default page to 'home'
   
 
   const featuredRolls = [
-    { name: "Dragon Roll", description: "Eel and cucumber inside with thinly sliced avocado on top.", price: 13.50, image: "image import name" },
-    // ... more featured rolls
+    { 
+      name: "Kokito", 
+      image: KokitoRoll
+    },
+    { 
+      name: "Rubi", 
+      image: RubiRoll
+    },
+    { 
+      name: "Mares", 
+      image: MaresRoll
+    },
+    { 
+      name: "Caterpillar", 
+      image: CaterpillarRoll
+    },
+    { 
+      name: "Fiji", 
+      image: FijiRoll
+    },
   ];
   return (
     <div className="w-full h-full mx-auto my-auto bg-gradient-to-r from-white to-gray-400">
-      <div className='shadow-lg w-full h-[100px] flex justify-between bg-gray-400 px-3 items-end mx-auto'>
+      <div className='shadow-lg w-full h-[100px] flex justify-between bg-gray-400 px-2 items-end mx-auto'>
         <div>
           <img src={Logo} alt='Logo' style={{width: '250px'}} />
         </div>
 
         <nav className="mb-2">
-          <button onClick={() => setCurrentPage('home')} className="mr-4">Home</button>
+          <button onClick={() => setCurrentPage('home')} className="mr-3">Home</button>
           <button onClick={() => setCurrentPage('menu')}>Menu</button>
         </nav>
       </div>
